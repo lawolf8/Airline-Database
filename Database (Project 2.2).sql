3f78018bf02154cd02eae002670f8b73cbb296b0
Luke Wolf <lawolf@usf.edu>
2024-03-31 15:25:40 -0400

    Update Database (Project 2.2).sql

--- Database (Project 2.2).sql
+++ Database (Project 2.2).sql
@@ -11,3 +11,13 @@ FROM (
 ) AS Subquery
 
 --Query 2
+SELECT COUNT(*) AS QueryCountCapacityFrom2017
+FROM (
+    SELECT F.flight_id
+    FROM flights F
+    JOIN planes P ON F.plane_id = P.plane_id
+    JOIN tickets T ON T.flight_id = F.flight_id
+    WHERE F.date BETWEEN '2017-01-01' AND '2017-12-31'
+    GROUP BY F.flight_id, P.capacity
+    HAVING (P.capacity * 0.25) > COUNT(T.ticket_id)
+) AS Subquery;
