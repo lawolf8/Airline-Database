754e51651522139288ef9ea53eae53312a333189
Luke Wolf <lawolf@usf.edu>
2024-03-31 15:17:17 -0400

    Update Database (Project 2.2).sql

--- Database (Project 2.2).sql
+++ Database (Project 2.2).sql
@@ -1,2 +1,13 @@
 --Group 8: Luke Wolf, Kyle Petrone, Michael Brennan, Reese Sleater
 --Query 1
+SELECT COUNT(*) AS QueryCountCapacity
+FROM (
+    SELECT F.flight_id
+    FROM flights F
+    JOIN planes P ON F.plane_id = P.plane_id
+    JOIN tickets T ON T.flight_id = F.flight_id
+    GROUP BY F.flight_id, P.capacity
+    HAVING (P.capacity * 0.5) < COUNT(T.ticket_id)
+) AS Subquery
+
+--Query 2
