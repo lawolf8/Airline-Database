932c1966ef6ca712a70419a9084f4a7d0cf2ab58
Sharticle <162065248+Sharticle@users.noreply.github.com>
2024-04-04 21:54:46 -0400

    Update Database (Project 2.2).sql

--- Database (Project 2.2).sql
+++ Database (Project 2.2).sql
@@ -107,19 +107,31 @@ ORDER BY
 
 
 --Query 7
-SELECT r.route_id,
-       w.name AS weekday,
-       DATEPART(HOUR, t.purchase_time) AS hour_of_day,
-       COUNT(*) AS num_tickets_sold
-FROM tickets t
-JOIN flights f ON t.flight_id = f.flight_id
-JOIN routes r ON f.route_id = r.route_id
-JOIN weekdays w ON r.weekday_id = w.weekday_id
-WHERE r.city_state_id_origin = (SELECT TOP 1 city_state_id FROM cities_states WHERE name = 'Orlando')
-  AND r.city_state_id_destination = (SELECT TOP 1 city_state_id FROM cities_states WHERE name = 'Tampa')
-GROUP BY r.route_id, w.name, DATEPART(HOUR, t.purchase_time)
-ORDER BY r.route_id, w.name, num_tickets_sold DESC;
-
+SELECT 
+    r.route_id,
+    w.name AS weekday,
+    DATEPART(HOUR, t.purchase_time) AS hour_of_day,
+    COUNT(*) AS num_tickets_sold
+FROM 
+    tickets t
+JOIN 
+    flights f ON t.flight_id = f.flight_id
+JOIN 
+    routes r ON f.route_id = r.route_id
+JOIN 
+    weekdays w ON r.weekday_id = w.weekday_id
+JOIN 
+    cities_states origin ON r.city_state_id_origin = origin.city_state_id
+JOIN 
+    cities_states destination ON r.city_state_id_destination = destination.city_state_id
+WHERE 
+    origin.name = 'Orlando'
+AND 
+    destination.name = 'Tampa'
+GROUP BY 
+    r.route_id, w.name, DATEPART(HOUR, t.purchase_time)
+ORDER BY 
+    r.route_id, w.name, num_tickets_sold DESC;
 --Query 8
 SELECT 
     f.flight_id,
